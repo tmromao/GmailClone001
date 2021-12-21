425f9251b07549865351c6fd8cd05824ce420ccd
tromao <tiagomromao@gmail.com>
2021-12-21 19:49:44 +0000

    AppBar menu design

--- app/src/main/java/com/example/gmailclone001/components/HomeAppBar.kt
+++ app/src/main/java/com/example/gmailclone001/components/HomeAppBar.kt
@@ -1,15 +1,27 @@
 package com.example.gmailclone001.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.gmailclone001.GmailApp
 import com.example.gmailclone001.ui.theme.GmailClone001Theme
+import com.example.gmailclone001.R
+
 
 @Composable
 fun HomeAppBar() {
@@ -21,8 +33,28 @@ fun HomeAppBar() {
             elevation = 6.dp
         ) {
 
-            Row(verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxSize().padding(8.dp)){
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp)
+            ) {
+
+                Icon(Icons.Default.Menu, "Menu")
+
+                Text(
+                    text = "Search in emails",
+                    modifier = Modifier.weight(2.0f)
+                )
+
+                Image(
+                    painter = painterResource(id = R.drawable.gradient_2),
+                    contentDescription = "Profile",
+                    modifier = Modifier
+                        .size(30.dp)
+                        .clip(CircleShape)
+                        .background(color = Color.Gray)
+                )
 
 
             }//ROW
